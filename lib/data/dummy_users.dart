3a3358d77d5f47c141779227d4a613d2b5b8726d
Felipe Andrade <feliperufini01@gmail.com>
2021-08-27 22:44:32 -0400

    Commit Inicial

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@gmail.com',
+    avatarUrl:
+        'https://64.media.tumblr.com/151476c45becfc9554ac8a6ab634f0cd/tumblr_qtv7j9nrIZ1rpwm80o1_250.jpg',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@gmail.com',
+    avatarUrl:
+        'https://png.pngitem.com/pimgs/s/338-3388366_meme-for-steam-avatars-hd-png-download.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernando',
+    email: 'fernando@gmail.com',
+    avatarUrl: 'https://i0.wp.com/pbs.twimg.com/media/DiHYZjOVAAA95Yc.jpg',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Felipe',
+    email: 'felipe@gmail.com',
+    avatarUrl: '',
+  ),
+};
